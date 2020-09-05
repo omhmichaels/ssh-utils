1bd2c8eb01cc6b7e5fa50850c616d9a3251af3e9
Michael Martin <omhmichaels@gmail.com>
2020-09-05 07:47:19 -0700

    TODO: Fix zsh breaks

--- generate-keys.sh
+++ generate-keys.sh
@@ -1,9 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 # Title: generate-keys.sh
 
 CONFIG=${1:- "~/.ssh/accounts.json"}
 printf "\nPLEASE ENTER A PASSWORD FOR SSH KEYS:\n";
-#read -s GOODIES;
+read -s GOODIES;
 
 # Use jq to get host from json config
 host_list=$(jq '.[] | .host ' ${CONFIG}) 
@@ -12,24 +12,30 @@ host_list=$(jq '.[] | .host ' ${CONFIG})
 user_list=$(jq '.[] | .user ' ${CONFIG}) 
 
 # Strip remaining quotes
-host_list=$(echo $host_list | xargs echo)  
+hosts=$(echo $host_list | xargs echo)  
 # Strip remaining quotes
-user_list=$(echo $user_list | xargs echo) 
+users=$(echo "${user_list}" | xargs echo) 
+
 
 echo HOSTS: $host_list
 echo USERS: $user_list
 # Loop through and generate keys
-for host user in $(echo $host_list) $(echo $user_list);
+IFS=$' '
+while read host;
 do
- 
+    printf "INDEX: $index"
     printf "\nACCOUNT:  $host\n"
-    printf "\nUSER:  $user\n"
+    printf "\nUSER:  ${user_list[$index]}\n"
 
-    KEYNAME="$host-$user"
+    KEYNAME="${host}-${user[$index]}"
     
     printf "\nKEYNAME: $KEYNAME\n"
-    printf "\nCREATING SSH KEY FOR $user\n"
-    #ssh-keygen -t rsa -b 4096 -P $GOODIES -f $KEYNAME 
+    printf "\nCREATING SSH KEY  $KEYNAME\n"
+    #ssh-keygen -t rsa -b 4096 -P $GOODIES -f "~/.ssh/$KEYNAME"
+
+    printf "\nAdding key to keychain\n"
+    #(echo $GOODIES) | ssh-add -K ~/.ssh/$KEYNAME
+    index=$(index + 1)
 done
 unset IFS
 printf "\nFINISHED GENERATING SSH KEYS.\n"
